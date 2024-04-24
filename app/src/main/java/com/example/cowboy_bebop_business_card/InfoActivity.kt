f8446905a1d8378aa2df3ef1140452ba8e291835
Iskandar Rassulov <iskander.rassulov.2002@gmail.com>
2024-04-24 21:24:49 +0200

    Fix: Ensure homeButton visibility by adjusting view order Reordered the components in the layout to ensure that the homeButton is visible above the viewPager. The button is now displayed correctly and is responsive to user interaction, facilitating intuitive navigation back to the MainActivity.

--- app/src/main/java/com/example/cowboy_bebop_business_card/InfoActivity.kt
+++ app/src/main/java/com/example/cowboy_bebop_business_card/InfoActivity.kt
@@ -4,6 +4,7 @@ import ScreenSlidePagerAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,18 +17,19 @@ class InfoActivity : AppCompatActivity(){
         enableEdgeToEdge()
         setContentView(R.layout.activity_info)
 
+        val homeButton: ImageButton = findViewById(R.id.homeButton)
+        homeButton.setOnClickListener {
+            Log.d("InfoActivity", "Home button clicked")
+            // Запуск MainActivity
+            val intent = Intent(this@InfoActivity, MainActivity::class.java)
+            // Если MainActivity уже запущена, вместо запуска нового экземпляра, просто покажем ее из стека
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            startActivity(intent)
+            finish()
+        }
+
         viewPager = findViewById(R.id.viewPager)
         viewPager.adapter = ScreenSlidePagerAdapter(this)
 
-//        val homeButton: ImageButton = findViewById(R.id.home_button)
-//        homeButton.setOnClickListener {
-//            Log.d("InfoActivity", "Home button clicked")
-//            // Запуск MainActivity
-//            val intent = Intent(this@InfoActivity, MainActivity::class.java)
-//            // Если MainActivity уже запущена, вместо запуска нового экземпляра, просто покажем ее из стека
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-//            startActivity(intent)
-//            finish()
-//        }
     }
 }
